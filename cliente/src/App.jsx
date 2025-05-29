7b77dd49b54bc6affafddcf4fb6272093bc2249b
RenzoQuispe <renzo123cd@gmail.com>
2025-05-29 08:39:02 -0500

    App.jsx setup inicial

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,8 +1,20 @@
+import {Route, Routes, Navigate} from "react-router";
+import {Toaster} from "react-hot-toast"
+//Pages
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage";
 
-function App() {
-  return (
-    <div>VideoChatSimple</div>
+const App = () => {
+  return(
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+      </Routes>
+      <Toaster/>
+    </div>
   )
 }
-
-export default App
+export default App;
