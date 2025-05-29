4e199505b8e8f3eeba075c4a4595c268962e1650
RenzoQuispe <renzo123cd@gmail.com>
2025-05-29 14:45:01 -0500

    bcryptjs en register()  auth.controllers.js

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -1,19 +1,29 @@
-
+import bcrypt from 'bcryptjs';
 import pool from '../libs/db.js';
 
 export const register = async (req, res) => {
     const { username, email, contraseña } = req.body;
-
+    // falta validacion y encriptacion
     try {
+        const hashedContraseña = await bcrypt.hash(contraseña,10);
         const result = await pool.query(
             'INSERT INTO usuarios (username, email, contraseña) VALUES ($1, $2, $3) RETURNING *',
-            [username, email, contraseña]
+            [username, email, hashedContraseña]
         );
         res.status(201).json({ "msg": "Creacion de usuario exitosa" });
     } catch (err) {
-        console.error(err);
+        console.error('Error al registrar usuario',err);
         res.status(500).json({ error: 'Error al registrar usuario' });
     }
 };
+export const actualizar = async (req, res) => {
+    const { username, constraseña } = req.body;
+    try {
+
+    } catch (err) {
+        res.status(500).json({error: 'Error al actualizar datos'})
+    }
+
+}
 
 
