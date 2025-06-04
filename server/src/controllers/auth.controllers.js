2eb4be3d05805c31c629768aca6ea97fbefca245
RenzoQuispe <renzo123cd@gmail.com>
2025-06-04 14:33:36 -0500

    login - backend

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import pool from '../libs/db.js';
+import { createAccessToken } from "../libs/jwt_utils.js"
 
 export const register = async (req, res) => {
     const { username, email, contraseña } = req.body;
@@ -54,5 +55,37 @@ export const actualizarUsuario = async (req, res) => {
         res.status(500).json({ message: 'Error del servidor' });
     }
 };
+export const login = async (req, res) => {
+    const { email, contraseña} = req.body;
+    try {
+        // buscar el usuario por su email
+        const result = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'No hay un usuario registrado con ese correo' });
+        }
+        const usuario = result.rows[0];
+        // comparar contraseñas usando bcrypt
+        const match = await bcrypt.compare(contraseña, usuario.contraseña);
 
+        if (!match) {
+            return res.status(401).json({ message: 'Contraseña incorrecta' });
+        }
+
+        createAccessToken(usuario.id, res);
+        res.status(200).json({
+            id: usuario.id,
+            username: usuario.username,
+            email: usuario.email,
+            fotoperfil: usuario.fotoperfil
+        });
+
+    } catch (error) {
+        console.log("Error login", error.message);
+        res.status(500).json({ message: "Server Error" });
+    }
+
+};
+export const logout = (req, res) => {
+
+};
 
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 // importar autenticacion controllers
-import { register,actualizarUsuario } from "../controllers/auth.controllers.js";
+import { login, register,actualizarUsuario } from "../controllers/auth.controllers.js";
 
 // enrutador
 const router = express.Router();
 // rutas
 router.post('/register',register);
 router.put('/actualizar/:id',actualizarUsuario);
+router.post('/login', login);
 
 export default router;
