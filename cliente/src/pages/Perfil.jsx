8825ca3c0cdf968d25d9268e749b95489d453015
RenzoQuispe <renzo123cd@gmail.com>
2025-06-07 22:18:04 -0500

    Perfil.jsx 1.0

--- cliente/src/pages/Perfil.jsx
+++ cliente/src/pages/Perfil.jsx
@@ -1,6 +1,14 @@
+import { estadoAuth } from "../estados/estadoAuth";
 const Perfil = () => {
+    const {authUser} = estadoAuth();
     return(
-        <div>Perfil</div>
+        <div>
+            <div>{authUser.id}</div>
+            <div>{authUser.username}</div>
+            <div>{authUser.email}</div>
+            <div>{authUser.fotoperfil}</div>
+            <div>{authUser.creado_en}</div>
+        </div>
     )
 }
 export default Perfil;
