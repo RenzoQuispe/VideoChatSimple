1201bf95c003789479fd5e17013aad82189530f1
RenzoQuispe <renzo123cd@gmail.com>
2025-07-13 14:26:18 -0500

    correccion de init.sql para importacion en docker-compose.yml

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,5 @@
--- Ejemplo de base de datos postgresql usada
--- Crear la base de datos y conectarse a ella antes de crear las tablas
-CREATE USER admin_videochat WITH PASSWORD '123456789';
-CREATE DATABASE videochat OWNER admin_videochat;
-GRANT ALL PRIVILEGES ON DATABASE videochat TO admin_videochat;
 -- Tabla de usuarios
-CREATE TABLE usuarios (
+CREATE TABLE IF NOT EXISTS usuarios (
   id SERIAL PRIMARY KEY,
   username TEXT UNIQUE NOT NULL,
   email TEXT UNIQUE NOT NULL,
@@ -13,7 +8,7 @@ CREATE TABLE usuarios (
   creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- Tabla de salas
-CREATE TABLE salas (
+CREATE TABLE IF NOT EXISTS salas (
     id SERIAL PRIMARY KEY,
     nombre TEXT UNIQUE NOT NULL,
     contraseña TEXT,  -- NULL si la sala no tiene contraseña
@@ -21,7 +16,7 @@ CREATE TABLE salas (
     creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- Relacion usuarios-salas (usuarios dentro de salas)
-CREATE TABLE usuarios_salas (
+CREATE TABLE IF NOT EXISTS usuarios_salas (
     usuario_id INTEGER REFERENCES usuarios(id) ON DELETE CASCADE,
     sala_id INTEGER REFERENCES salas(id) ON DELETE CASCADE,
     es_admin BOOLEAN DEFAULT FALSE,
@@ -29,7 +24,7 @@ CREATE TABLE usuarios_salas (
     PRIMARY KEY (usuario_id, sala_id)
 );
 -- tabla para chat general en la sala
-CREATE TABLE mensajes (
+CREATE TABLE IF NOT EXISTS mensajes (
   id SERIAL PRIMARY KEY,
   sala_id INTEGER REFERENCES salas(id) ON DELETE CASCADE,
   usuario_id INTEGER REFERENCES usuarios(id) ON DELETE SET NULL,
@@ -41,14 +36,13 @@ GRANT ALL PRIVILEGES ON TABLE salas TO admin_videochat;
 GRANT ALL PRIVILEGES ON TABLE mensajes TO admin_videochat;
 GRANT ALL PRIVILEGES ON TABLE usuarios TO admin_videochat;
 GRANT ALL PRIVILEGES ON TABLE usuarios_salas TO admin_videochat;
+-- Crear usuarios de prueba
+INSERT INTO usuarios (username, email, contraseña) VALUES 
+('usuario1', 'user1@example.com', '123'),
+('usuario2', 'user2@example.com', '123'),
+('usuario3', 'user3@example.com', '123');
 -- Crear salas de prueba
-INSERT INTO salas (nombre, contraseña, creado_por) VALUES ('Sala1', 'abc123', 14);
-INSERT INTO salas (nombre, contraseña, creado_por) VALUES ('Sala2', NULL,15);
-INSERT INTO salas (nombre, contraseña, creado_por) VALUES ('Sala3', 'claveSegura', 16);
-INSERT INTO salas (nombre, contraseña, creado_por) VALUES ('Sala4', NULL, 17);
-INSERT INTO salas (nombre, contraseña, creado_por) VALUES ('Sala5', 'sala123', 18);
-INSERT INTO salas (nombre, contraseña, creado_por) VALUES ('Sala6', NULL, 19);
-INSERT INTO salas (nombre, contraseña, creado_por) VALUES ('Sala7', 'qwerty', 20);
-INSERT INTO salas (nombre, contraseña, creado_por) VALUES ('Sala8', NULL, 21);
-INSERT INTO salas (nombre, contraseña, creado_por) VALUES ('Sala9', 'xyz789', 22);
-INSERT INTO salas (nombre, contraseña, creado_por) VALUES ('Sala10', NULL, 23);
+INSERT INTO salas (nombre, contraseña, creado_por) VALUES 
+('Sala1', 'abc123', 1),
+('Sala2', NULL, 2),
+('Sala3', 'claveSegura', 3);
