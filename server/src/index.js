00fd9a4c41e461f3b996fc155901a943c6fc24dc
RenzoQuispe <renzo123cd@gmail.com>
2025-07-13 14:27:12 -0500

    configuracion de CORS para acceso de dispositivos en red local

--- server/src/index.js
+++ server/src/index.js
@@ -14,21 +14,34 @@ const PORT_SERVER = process.env.PORT_SERVER;
 
 // Configurar app
 const app = express();
-//cors
-app.use(
-    cors({
-        origin: true,
-        credentials: true
-    })
-);
-//
+
+// CORS
+const corsOptions = {
+    origin: function (origin, callback) {
+
+        if (!origin) return callback(null, true);
+
+        if (origin.startsWith("http://localhost:5173")) return callback(null, true);
+
+        const regexRedLocal = /^http:\/\/192\.168\.1\.\d{1,3}:5173$/;
+
+        if (regexRedLocal.test(origin)) return callback(null, true);
+
+        return callback(new Error("CORS no permitido desde este origen: " + origin));
+
+    },
+    credentials: true
+};
+app.use(cors(corsOptions));
+
+
 app.use(cookieParser());
-app.use(express.json({limit:'50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 //rutas
 app.use("/api/auth", authRoutes);
-app.use("/api/sala",salaRoutes);
+app.use("/api/sala", salaRoutes);
 const rutaUploads = process.env.DB_RUTA_IMAGENES;
 app.use('/uploads', express.static(rutaUploads));
 // Leer los certificados
@@ -37,11 +50,7 @@ const cert = fs.readFileSync('./certs/cert.pem');
 
 // Crear el server HTTPS
 const server = https.createServer({ key, cert }, app);
-server.listen(PORT_SERVER,'0.0.0.0',() =>{
-    console.log('Server HTTPS ejecutandose en el puerto '+PORT_SERVER+'. Escuchando en:');
-    console.log('https://localhost')
-    console.log('https://127.0.0.1') 
-    console.log('https://videochat.local')
-    console.log('https://miapp.local') 
+server.listen(PORT_SERVER, '0.0.0.0', () => {
+    console.log('Server HTTPS ejecutandose en el puerto ' + PORT_SERVER ); // En mi caso escuchando en videochat.local y 192.168.1.10
     probarConexionDB()
 })
